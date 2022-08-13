6bba9038fb2615175443233cc9744d9afdf12585
Md Shariful Islam <shaarifultarsi51@gmail.com>
2022-08-13 14:07:07 +0600

    devolper update

--- about_us.php
+++ about_us.php
@@ -7,24 +7,9 @@ include 'assets/inc/header.php';
 
 <div class="container my-5">
     <hr>
-    <h1 class="text-center">Our Team Member</h1>
+    <h1 class="text-center">Devoloped By</h1>
     <hr>
-    <div class="row" my-2>
-        <div class="col-md-2">
-        </div>
-        <div class="col-md-4 mt-2 ">
-            <img height="220px" width="220px" src="assets/images/SZH_3113.jpg" alt="">
-        </div>
-        <div class="col-md-4 mt-4">
-            <h3>SYED ABU ZAYED HOSSAIN</h3>
-            <h5>Full Stack Devoloper</h5>
-            <h6>Dhaka, Bangladesh</h6>
-            <h6>Email: zayedbd24@gmail.com</h6>
-            <h6>Contact: 01629-999666</h6>
-        </div>
-        <div class="col-md-2">
-        </div>
-    </div>
+    
     <div class="row" my-2>
 
         <div class="col-md-2">
@@ -46,72 +31,7 @@ include 'assets/inc/header.php';
 
         </div>
     </div>
-    <div class="row" my-2>
 
-        <div class="col-md-2">
-
-        </div>
-        <div class="col-md-4 mt-2">
-            <img height="220px" width="220px" src="assets/images/Mohsin.JPG" alt="">
-        </div>
-        <div class="col-md-4 mt-4">
-            <h3> MOHSIN AHMED</h3>
-            <h5>
-                Full Stack Devoloper
-            </h5>
-            <h6>Dhaka, Bangladesh</h6>
-            <h6>Email: mohsinahmed0000@gmail.com</h6>
-            <h6>Contact: 01919-112153</h6>
-        </div>
-
-        <div class="col-md-2">
-
-        </div>
-    </div>
-    <div class="row" my-2>
-
-        <div class="col-md-2">
-
-        </div>
-        <div class="col-md-4 mt-2">
-            <img height="220px" width="220px" src="assets/images/Tasnim.jpeg" alt="">
-        </div>
-        <div class="col-md-4 mt-4">
-            <h3> TASNIM AL RAHMAN</h3>
-            <h5>
-                Full Stack Devoloper
-            </h5>
-            <h6>Dhaka, Bangladesh</h6>
-            <h6>Email: tasnim1994alrahman@gmail.com</h6>
-            <h6>Contact: 01904-325619</h6>
-        </div>
-
-        <div class="col-md-2">
-
-        </div>
-    </div>
-    <div class="row" my-2>
-
-        <div class="col-md-2 ">
-
-        </div>
-        <div class="col-md-4 mt-2">
-            <img height="220px" width="220px" src="assets/images/Tamim.jpg" alt="">
-        </div>
-        <div class="col-md-4 mt-4">
-            <h3> MD TAMIMUL ISLAM</h3>
-            <h5>
-                Full Stack Devoloper
-            </h5>
-            <h6>Dhaka, Bangladesh</h6>
-            <h6>Email: tamimislam732@gmail.com</h6>
-            <h6>Contact: 01911-151732</h6>
-        </div>
-
-        <div class="col-md-2">
-
-        </div>
-    </div>
 
 </div>
 </div>
