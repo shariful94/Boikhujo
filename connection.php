d63f78224f46b366cb0081867ac0ac833bb573f6
Md Shariful Islam <shaarifultarsi51@gmail.com>
2022-08-31 10:53:08 +0600

    details and conection have to modified, 60% done

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "configuration.php";
-
-$conn = new mysqli($sitename, $username, $password, $database) or die("Connection failed: " . $conn->connect_error);
-$conn->set_charset("utf8");
